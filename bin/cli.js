110b7c1cb41b4507fd193ef7c9bb681d3edb605c
Bramus <bramus@bram.us>
2022-03-10 21:35:00 +0100

    ✨ Expose the CLI script — Fix #1

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { calculate } from './index.js';
+import { calculate } from '../dist/index.js';
 
 if (!process.argv[2]) {
     console.error('❌ Missing selector argument');
