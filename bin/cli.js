413df6c3c3def21bc96b9fed3e9b7a3aa702ff28
Bramus <bramus@bram.us>
2022-03-22 22:08:28 +0100

    🚑 Fix binary

--- bin/cli.js
+++ bin/cli.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { calculate } from '../dist/index.js';
+import Specificity from '../dist/index.js';
 
 if (!process.argv[2]) {
     console.error('❌ Missing selector argument');
@@ -7,7 +7,7 @@ if (!process.argv[2]) {
 }
 
 try {
-    const specificities = calculate(process.argv[2]);
+    const specificities = Specificity.calculate(process.argv[2]);
     console.log(specificities.map((specificity) => `${specificity}`).join('\n'));
 } catch (e) {
     console.error(`❌ ${e.message}`);
